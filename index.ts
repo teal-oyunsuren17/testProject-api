1d94e3cd5fddd2eb148fc1a227a1a0495c3ca7d0
teal-oyunsuren17 <teal.2022.oyunsuren.purevsuren@gmail.com>
2023-04-04 16:58:26 +0800

    test

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 8000;
 app.get('/', (req: Request, res: Response) => {
-res.send('Express + TypeScript Server ');
+res.send('Express + TypeScript Server 8658658');
 });
 app.listen(port, () => {
 console.log(`⚡️ [server]: Server is running at http://localhost:${port}`)});
