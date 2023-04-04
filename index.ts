c6c56c366bdbf9e2e1b5f4d9b4cc07cca5b39688
teal-oyunsuren17 <teal.2022.oyunsuren.purevsuren@gmail.com>
2023-04-04 17:34:28 +0800

    test mongoose

--- index.ts
+++ index.ts
@@ -1,10 +1,33 @@
 import express, { Express, Request, Response } from 'express';
+const mongoose = require("mongoose");
 import dotenv from 'dotenv';
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 8000;
-app.get('/', (req: Request, res: Response) => {
-res.send('Express + TypeScript Server 8658658');
-});
+
+mongoose
+  .connect(
+    "mongodb+srv://teal2022oyunsurenpurevsuren:GMNqs2612i2U1wjN@cluster0.bqab1l0.mongodb.net/blog"
+  )
+  .then(() => console.log("Connected"));
+
+// app.get('/', (req: Request, res: Response) => {
+// res.send('Express + TypeScript Server 8658658');
+// });
+
+const categorySchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+  });
+  
+  const Category = mongoose.model("Category", categorySchema);
+  app.use("/category", CategoryLoad)
+
+function CategoryLoad() {
+    app.get("/" , (req : Request, res : Response) => {
+        const list : [] =  Category.find();
+  res.json(list);
+    }) 
+}
 app.listen(port, () => {
 console.log(`⚡️ [server]: Server is running at http://localhost:${port}`)});
