964b70d7a03609918120e0d131190d164594b301
teal-oyunsuren17 <teal.2022.oyunsuren.purevsuren@gmail.com>
2023-04-05 10:54:28 +0800

    try to connect mongoose

--- index.ts
+++ index.ts
@@ -1,33 +1,31 @@
 import express, { Express, Request, Response } from 'express';
 const mongoose = require("mongoose");
 import dotenv from 'dotenv';
+import { model, Schema } from 'mongoose';
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 8000;
 
 mongoose
   .connect(
-    "mongodb+srv://teal2022oyunsurenpurevsuren:GMNqs2612i2U1wjN@cluster0.bqab1l0.mongodb.net/blog"
+    "mongodb+srv://teal2022oyunsurenpurevsuren:GMNqs2612i2U1wjN@cluster0.bqab1l0.mongodb.net/sample_mflix"
   )
   .then(() => console.log("Connected"));
 
-// app.get('/', (req: Request, res: Response) => {
-// res.send('Express + TypeScript Server 8658658');
-// });
 
 const categorySchema = new mongoose.Schema({
     _id: String,
     name: String,
   });
-  
-  const Category = mongoose.model("Category", categorySchema);
-  app.use("/category", CategoryLoad)
+const Category = mongoose.model("Category", categorySchema);
 
-function CategoryLoad() {
-    app.get("/" , (req : Request, res : Response) => {
-        const list : [] =  Category.find();
-  res.json(list);
+    app.get("/category" , (req : Request, res : Response) => {
+        const list  =  Category.find();
+        res.json(list);
     }) 
-}
+
 app.listen(port, () => {
 console.log(`⚡️ [server]: Server is running at http://localhost:${port}`)});
+
+
+
